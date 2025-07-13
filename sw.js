84a24592a8dce68e3da09ffbc96924f824642a3d
Aravindjogi <aravindjogi454@gmail.com>
2025-07-13 15:44:43 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("portfolio-cache").then((cache) =>
+      cache.addAll(["/", "/index.html", "/output.css"])
+    )
+  );
+});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
